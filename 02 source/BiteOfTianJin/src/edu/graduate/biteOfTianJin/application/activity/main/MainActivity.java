9d4d771f96976b8768363944e796c63572d8025c
joaquinaimar <joaquinaimar@163.com>
2014-04-08 22:50:29 +0800

    2014.4.8

--- 02 source/BiteOfTianJin/src/edu/graduate/biteOfTianJin/application/activity/main/MainActivity.java
+++ 02 source/BiteOfTianJin/src/edu/graduate/biteOfTianJin/application/activity/main/MainActivity.java
@@ -1,24 +1,17 @@
-package edu.graduate.biteOfTianJin.application.activity;
-
-import java.util.ArrayList;
-import java.util.HashMap;
+package edu.graduate.biteOfTianJin.application.activity.main;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.SimpleAdapter;
 import edu.graduate.biteOfTianJin.R;
-import edu.graduate.biteOfTianJin.application.listener.SearchViewQueryTextListener;
+import edu.graduate.biteOfTianJin.application.activity.main.listener.SearchViewQueryTextListener;
 import edu.graduate.biteOfTianJin.basic.ExitApplication;
 
 public class MainActivity extends Activity {
 
 	private SearchView searchView = null;
-
-	private ListView funListView = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +22,6 @@ public class MainActivity extends Activity {
 		searchView
 				.setOnQueryTextListener(new SearchViewQueryTextListener(this));
 		searchView.setSubmitButtonEnabled(false);
-
-		funListView = (ListView) findViewById(R.id.listResult);
-
-		getResultList();
 
 	}
 
@@ -52,21 +41,4 @@ public class MainActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	private void getResultList() {
-
-		ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String, Object>>();
-		for (int i = 0; i < 10; i++) {
-			HashMap<String, Object> map = new HashMap<String, Object>();
-			map.put("listContent", i + "");
-			map.put("listButton", "È·¶¨");
-			listItem.add(map);
-		}
-
-		SimpleAdapter listItemAdapter = new SimpleAdapter(this, listItem,
-				R.layout.result_listview, new String[] { "listContent",
-						"listButton" }, new int[] { R.id.listContent,
-						R.id.listButton });
-		funListView.setAdapter(listItemAdapter);
-
-	}
 }
---
+++ 02 source/BiteOfTianJin/src/edu/graduate/biteOfTianJin/application/activity/main/listener/ListItemClickListener.java
@@ -0,0 +1,24 @@
+package edu.graduate.biteOfTianJin.application.activity.main.listener;
+
+import edu.graduate.biteOfTianJin.basic.DialogUtil;
+import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class ListItemClickListener implements OnItemClickListener {
+
+	private Activity activity = null;
+
+	public ListItemClickListener(Activity activity) {
+		this.activity = activity;
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		DialogUtil.alert(activity, position + "");
+
+	}
+
+}
