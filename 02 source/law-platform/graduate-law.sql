567be7e40a4b3fb99f5100086e1f7fda040e5686
joaquinaimar <joaquinaimar@163.com>
2014-04-20 21:46:48 +0800

    添加律师事务所项目sql
    Signed-off-by: joaquinaimar <joaquinaimar@163.com>

--- 02 source/law-platform/graduate-law.sql
+++ 02 source/law-platform/graduate-law.sql
@@ -10,10 +10,65 @@ Target Server Type    : MYSQL
 Target Server Version : 50520
 File Encoding         : 65001
 
-Date: 2014-04-18 16:09:49
+Date: 2014-04-20 21:45:44
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for lawyer
+-- ----------------------------
+DROP TABLE IF EXISTS `lawyer`;
+CREATE TABLE `lawyer` (
+  `id` varchar(36) NOT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `skill` varchar(20) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `summary` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of lawyer
+-- ----------------------------
+INSERT INTO `lawyer` VALUES ('1', '段浩', '经济', '1231', 'abc@163.com', '100万项目');
+
+-- ----------------------------
+-- Table structure for lawyer_detail
+-- ----------------------------
+DROP TABLE IF EXISTS `lawyer_detail`;
+CREATE TABLE `lawyer_detail` (
+  `id` varchar(36) NOT NULL,
+  `lawyer_id` varchar(36) DEFAULT NULL,
+  `resume` varchar(1000) DEFAULT NULL,
+  `result` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of lawyer_detail
+-- ----------------------------
+INSERT INTO `lawyer_detail` VALUES ('1', '1', '1000万', '失败');
+INSERT INTO `lawyer_detail` VALUES ('2', '1', '110万', '成功');
+
+-- ----------------------------
+-- Table structure for reserve
+-- ----------------------------
+DROP TABLE IF EXISTS `reserve`;
+CREATE TABLE `reserve` (
+  `id` varchar(36) NOT NULL,
+  `lawyer_id` varchar(36) DEFAULT NULL,
+  `user_id` varchar(36) DEFAULT NULL,
+  `content` varchar(1000) DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of reserve
+-- ----------------------------
+INSERT INTO `reserve` VALUES ('4028d681457ef84101457ef956ff0000', '1', '1', '和双创打官司', '2014-04-20 19:50:41');
 
 -- ----------------------------
 -- Table structure for user
@@ -23,10 +78,18 @@ CREATE TABLE `user` (
   `id` varchar(36) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(36) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'admin', '123');
+INSERT INTO `user` VALUES ('1', 'admin', '123', '33333', 'fsdfdfds');
+
+-- ----------------------------
+-- View structure for reserve_manage
+-- ----------------------------
+DROP VIEW IF EXISTS `reserve_manage`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `reserve_manage` AS select `r`.`id` AS `id`,`r`.`lawyer_id` AS `lawyer_id`,`r`.`user_id` AS `user_id`,`r`.`content` AS `content`,`r`.`time` AS `time`,`l`.`name` AS `lawyer_name`,`u`.`username` AS `user_name` from ((`reserve` `r` join `lawyer` `l` on((`l`.`id` = `r`.`lawyer_id`))) join `user` `u` on((`u`.`id` = `r`.`user_id`))) ;
