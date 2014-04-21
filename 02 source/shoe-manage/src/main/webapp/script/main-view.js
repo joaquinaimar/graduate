ff49a63f71683592f885eab05e6344a95a937e9c
lizhi <joaquinaimar@163.com>
2014-04-21 15:09:20 +0800

    提交轮胎问题
    Signed-off-by: lizhi <joaquinaimar@163.com>

--- 02 source/shoe-manage/src/main/webapp/script/main-view.js
+++ 02 source/shoe-manage/src/main/webapp/script/main-view.js
@@ -8,68 +8,82 @@ Ext.onReady(function() {
 		layout : 'fit',
 		items : mainPanel
 	});
+
+	changePage("/customer");
 });
 
-shoe.main.MainPanel = Ext.extend(Ext.Panel, {
-	id : 'mainPanel',
-	layout : 'border',
-	items : [{
-		id : 'mainHead',
-		region : 'north',
-		height : 70,
-		layout : 'border',
-		items : [{
-			xtype : 'panel',
-			width : 400,
-			region : 'west',
-			html : '<img src="' + contextPath + '/image/logo.png" width=100% height=100%>'
-		}, {
-			xtype : 'panel',
-			region : 'center',
-			layout : {
-				type : 'hbox',
-				align : 'stretch',
-				pack : 'start'
-			},
-			defaults : {
-				xtype : 'button',
-				flex : 10
-			},
-			items : [{
-				text : '客户管理',
-				onClick : function() {
-					changePage("/customer");
-				}
-			}, {
-				text : '销售记录',
-				onClick : function() {
-					changePage("/sell");
-				}
-			}, {
-				text : '轮胎信息统计',
-				onClick : function() {
-					changePage("/shoe");
-				}
-			}, {
-				text : '售后管理',
-				onClick : function() {
-					changePage("/afterService");
-				}
-			}]
-		}]
-	}, {
-		id : 'mainMain',
-		layout : 'fit',
-		region : 'center',
-		items : [{
-			xtype : 'container',
-			layout : 'fit',
-			autoEl : {
-				tag : 'iframe',
-				name : 'mainpage',
-				frameBorder : 0
-			}
-		}]
-	}]
+shoe.main.MainPanel = Ext
+		.extend(
+				Ext.Panel,
+				{
+					id : 'mainPanel',
+					layout : 'border',
+					items : [
+							{
+								id : 'mainHead',
+								region : 'north',
+								height : 70,
+								layout : 'border',
+								items : [
+										{
+											xtype : 'panel',
+											width : 400,
+											region : 'west',
+											html : '<img src="'
+													+ contextPath
+													+ '/image/logo.png" width=100% height=100%>'
+										},
+										{
+											xtype : 'panel',
+											region : 'center',
+											layout : {
+												type : 'hbox',
+												align : 'stretch',
+												pack : 'start'
+											},
+											defaults : {
+												xtype : 'button',
+												flex : 10
+											},
+											items : [
+													{
+														text : '客户管理',
+														onClick : function() {
+															changePage("/customer");
+														}
+													},
+													{
+														text : '销售记录',
+														onClick : function() {
+															changePage("/sell");
+														}
+													},
+													{
+														text : '轮胎信息统计',
+														onClick : function() {
+															changePage("/shoe");
+														}
+													},
+													{
+														text : '售后管理',
+														onClick : function() {
+															changePage("/afterService");
+														}
+													} ]
+										} ]
+							}, {
+								id : 'mainMain',
+								layout : 'fit',
+								region : 'center',
+								items : [ {
+									xtype : 'container',
+									layout : 'fit',
+									autoEl : {
+										tag : 'iframe',
+										name : 'mainpage',
+										frameBorder : 0
+									}
+								} ]
+							} ]
 
-});
+				});
